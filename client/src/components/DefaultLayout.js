5a93b7ef416f54fb954f44194f0cfd26da5980bb
NickBwalley <nickbwalley@gmail.com>
2022-01-05 23:06:12 +0300

    front-end setUp

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function DefaultLayout(props) {
+    return (
+        <div>
+            <div className='header bs1'>
+                <div className='d-flex justify-content-between'>
+                    <h1>NickyCarz</h1>
+
+                    <button>user</button>
+                </div> 
+            </div> 
+
+            <div className='content'> 
+                {props.children}
+            </div> 
+
+        </div>
+    )
+}
+
+export default DefaultLayout
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
 
 function Home() {
     return (
-        <div>
+        <DefaultLayout>
             <h1>Home Page</h1>
             <h2>We are here with</h2>
-        </div>
+        </DefaultLayout>
     )
 }
 

474021c8bd9eec14353e361de8e38525b48ad3f4
NickBwalley <nickbwalley@gmail.com>
2022-02-06 17:52:02 +0300

    homepage design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
+import {useSelector} from 'react-redux'
 
 function Home() {
+    const {cars} = useSelector(state=>state.carsReducer)
     return (
         <DefaultLayout>
             <h1>Home Page</h1>
-            <h2>We are here with</h2>
+            <h2>The length of cars array is {cars.length}</h2>
         </DefaultLayout>
     )
 }
